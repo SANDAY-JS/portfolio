1913399d811fc2621a89283ab2dd9431e5eae352
Sun Higuchi <san.sh.s3@outlook.jp>
2021-05-06 23:24:45 +0900

    updated on May 6.

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // ----
 // DOMs
 // ----
+let scrollPos = 0;
+
 const hamburgerBackground = document.querySelector(".hamburger-background");
 const headerMenuContainer = document.querySelector(".header-menu-container");
 const line = document.querySelectorAll(".line");
@@ -18,26 +20,23 @@ const toVoice = document.querySelector("#to-voice");
 const toPayment = document.querySelectorAll("#to-payment");
 const toContact = document.querySelector("#to-contact");
 
-let aboutMeParent = document.querySelector(".about-me-container");
 const aboutMe = document.querySelector("#about-me");
 const myProjects = document.querySelector("#my-projects");
 const supportContents = document.querySelector("#support-contents");
 const voice = document.querySelector("#voice");
 const payment = document.querySelector("#payment");
-// const contact = document.querySelector("#contact");
 
-// Slide Animation Elements
 const slideInElements = document.querySelectorAll("#slideInElement");
 
 /*--------------------
 -------- Methods -----
 -------------------- */
-function click() {
+function showHeaderMenu() {
   headerMenuContainer.classList.toggle("click");
   hamburgerBackground.classList.toggle("click");
 }
 
-function deltaFunction() {
+function detectScrollWay() {
   let delta;
 
   if (event.wheelDelta) {
@@ -54,27 +53,36 @@ function deltaFunction() {
   }
 }
 
-let scrollPos = 0;
-
 function topBtnDisplay() {
   // detects new state and compares it with the new one
   if (document.body.getBoundingClientRect().top > scrollPos) {
     toTopBtn.style.display = "block";
-  } else {
-    toTopBtn.style.display = "none";
+    // saves the new position for iteration.
+    scrollPos = document.body.getBoundingClientRect().top;
+    return;
   }
 
+  toTopBtn.style.display = "none";
   // saves the new position for iteration.
   scrollPos = document.body.getBoundingClientRect().top;
 }
 
 function findScrollDirectionOtherBrowsers() {
   if (window.matchMedia("(min-width: 768px)").matches) {
-    deltaFunction();
+    detectScrollWay();
   }
 }
 
-function hoverAnimationMove(e) {
+function nameHoverAnimationIn() {
+  SunHiguchi.style.transition = "none";
+  smallText.style.transition = "none";
+
+  //   Popout
+  SunHiguchi.style.transform = "translateZ(100px)";
+  smallText.style.transform = "translateZ(100px) rotateZ(-40deg)";
+}
+
+function nameHoverAnimationMove(e) {
   let xAxis = (window.innerWidth - e.pageX) / 10;
   let yAxis = (window.innerHeight - e.pageY) / 10;
 
@@ -84,16 +92,7 @@ function hoverAnimationMove(e) {
     "rotateY(" + yAxis + "deg) rotateX( " + xAxis + "deg)";
 }
 
-function hoverAnimationIn(e) {
-  SunHiguchi.style.transition = "none";
-  smallText.style.transition = "none";
-
-  //   Popout
-  SunHiguchi.style.transform = "translateZ(100px)";
-  smallText.style.transform = "translateZ(100px) rotateZ(-40deg)";
-}
-
-function hoverAnimationOut() {
+function nameHoverAnimationOut() {
   SunHiguchi.style.transition = "all .3s ease";
   SunHiguchi.style.transform = "rotateY(0deg) rotateX(0deg)";
   smallText.style.transition = "all .3s ease";
@@ -108,47 +107,15 @@ function toTheTop() {
   window.scrollTo(0, 0);
 }
 
-function toAboutSection() {
-  // aboutMeParent = aboutMe.offsetParent;
-  const aboutMeHeight = aboutMe.getBoundingClientRect().top;
-  const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-  window.scrollTo(0, aboutMeHeight + scrollTop);
-  console.log(aboutMeHeight + scrollTop);
-}
-
-function toProjectsSection() {
-  const myProjectsHeight = myProjects.offsetTop - 30;
-  window.scrollTo(0, myProjectsHeight);
-}
-
-function toSupportsSection() {
-  const supportContentsHeight = supportContents.offsetTop - 30;
-  window.scrollTo(0, supportContentsHeight);
-}
-
-function toVoiceSection() {
-  const voiceHeight = voice.offsetTop - 30;
-  window.scrollTo(0, voiceHeight);
-}
-
-function toPaymentSection() {
-  const paymentHeight = payment.offsetTop - 30;
-  window.scrollTo(0, paymentHeight);
-}
-
-function toContactSection() {
-  const contactHeight =
-    document.documentElement.scrollHeight -
-    document.documentElement.clientHeight +
-    1000;
-  window.scroll(0, contactHeight);
-}
-
 function changeMenuColor() {
+  const aboutMeHeight = aboutMe.getBoundingClientRect().top + 50;
+  const contactHeight = payment.getBoundingClientRect().bottom;
+
   if (
-    (document.body.scrollTop > 550 && document.body.scrollTop <= 4300) ||
-    (document.documentElement.scrollTop > 550 &&
-      document.documentElement.scrollTop <= 4300)
+    (document.body.scrollTop > aboutMeHeight &&
+      document.body.scrollTop <= contactHeight) ||
+    (document.documentElement.scrollTop > aboutMeHeight &&
+      document.documentElement.scrollTop <= contactHeight)
   ) {
     for (let item = 0; item < line.length; item++) {
       const lines = line[item];
@@ -160,17 +127,18 @@ function changeMenuColor() {
       menuItems.classList.remove("color-default");
       menuItems.classList.add("color-not-default");
     }
-  } else {
-    for (let item = 0; item < line.length; item++) {
-      const lines = line[item];
-      lines.style.backgroundColor = "#ddd";
-    }
+    return;
+  }
 
-    for (let item = 0; item < menuItem.length; item++) {
-      const menuItems = menuItem[item];
-      menuItems.classList.remove("color-not-default");
-      menuItems.classList.add("color-default");
-    }
+  for (let item = 0; item < line.length; item++) {
+    const lines = line[item];
+    lines.style.backgroundColor = "#ddd";
+  }
+
+  for (let item = 0; item < menuItem.length; item++) {
+    const menuItems = menuItem[item];
+    menuItems.classList.remove("color-not-default");
+    menuItems.classList.add("color-default");
   }
 }
 
@@ -192,32 +160,74 @@ function slideElementsInWindow() {
   }
 }
 
+// Classes
+class GoToSpecifiedSection {
+  constructor(el) {
+    this.el = el;
+  }
+  goToElHeight() {
+    const elHeight = this.el.offsetTop - 30;
+    window.scrollTo(0, elHeight);
+  }
+  goToAboutSection() {
+    const aboutMeHeight = aboutMe.getBoundingClientRect().top;
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    window.scrollTo(0, aboutMeHeight + scrollTop);
+  }
+  goToContactSection() {
+    const contactHeight =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight +
+      1000;
+    window.scroll(0, contactHeight);
+  }
+}
+
+function toTheSection(el) {
+  new GoToSpecifiedSection(el).goToElHeight();
+}
+function toAboutSection(el) {
+  new GoToSpecifiedSection(el).goToAboutSection();
+}
+function toContactSection(el) {
+  new GoToSpecifiedSection(el).goToContactSection();
+}
+
 // ------
 // Events
 // ------
 
-hamburgerBackground.addEventListener("click", click);
+hamburgerBackground.addEventListener("click", showHeaderMenu);
 
+// Go to Each Section
 // For IE
 const toAboutArray = Array.prototype.slice.call(toAbout);
 toAboutArray.forEach(function (toAboutBtn) {
   toAboutBtn.addEventListener("click", toAboutSection);
 });
-
-toTopBtn.addEventListener("click", toTheTop);
-toProjects.addEventListener("click", toProjectsSection);
-toSupports.addEventListener("click", toSupportsSection);
-toVoice.addEventListener("click", toVoiceSection);
-
 const toPaymentArray = Array.prototype.slice.call(toPayment);
 toPaymentArray.forEach(function (paymentBtn) {
-  paymentBtn.addEventListener("click", toPaymentSection);
+  paymentBtn.addEventListener("click", function () {
+    toTheSection(payment);
+  });
+});
+
+toTopBtn.addEventListener("click", toTheTop);
+toProjects.addEventListener("click", function () {
+  toTheSection(myProjects);
+});
+toSupports.addEventListener("click", function () {
+  toTheSection(supportContents);
+});
+toVoice.addEventListener("click", function () {
+  toTheSection(voice);
 });
 toContact.addEventListener("click", toContactSection);
 
-hoverCard.addEventListener("mousemove", hoverAnimationMove);
-hoverCard.addEventListener("mouseenter", hoverAnimationIn);
-hoverCard.addEventListener("mouseleave", hoverAnimationOut);
+// Top Name Hover Animation
+hoverCard.addEventListener("mousemove", nameHoverAnimationMove);
+hoverCard.addEventListener("mouseenter", nameHoverAnimationIn);
+hoverCard.addEventListener("mouseleave", nameHoverAnimationOut);
 
 window.addEventListener("wheel", findScrollDirectionOtherBrowsers);
 window.addEventListener("scroll", changeMenuColor);
