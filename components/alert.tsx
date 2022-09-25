34a99da1e0b64c88384167accb61e60ca9fa3bca
SANDAY-JS <san.sh.s3@outlook.jp>
2022-09-25 12:13:29 +0900

    Renamed Files in Components

--- components/alert.tsx
+++ components/alert.tsx
@@ -1,4 +1,4 @@
-import Container from './container'
+import Container from './Container'
 import cn from 'classnames'
 import { EXAMPLE_PATH } from '../lib/constants'
 
@@ -25,14 +25,7 @@ export default function Alert({ preview }) {
             </>
           ) : (
             <>
-              The source code for this blog is{' '}
-              <a
-                href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
-                className="underline hover:text-success duration-200 transition-colors"
-              >
-                available on GitHub
-              </a>
-              .
+              <p>Headerではない</p>
             </>
           )}
         </div>
