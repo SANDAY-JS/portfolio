a3201191f4fa06006c5ec527a9463ae5664312ce
SANDAY-JS <san.sh.s3@outlook.jp>
2022-09-25 21:41:45 +0900

    Adjusted text size & layouta

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,10 +10,10 @@ export default function Header({pages}: Props) {
   const isPageWide = useMediaQuery('(min-width: 768px)')
 
   return (
-    <div className="flex justify-between py-1 md:py-0 px-2 md:px-0 sticky top-0 left-0 z-50 bg-accent-1">
+    <div className="flex justify-between py-1 md:py-0 sticky top-0 left-0 z-50 bg-accent-1 mb-4 md:mb-8">
       <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight">
         <Link href="/">
-          <a className="hover:text-accent-2 inline-block">
+          <a className="hover:text-accent-2 flex">
             <Image src={isPageWide ? '/images/logo&name.png' : '/images/logo.png'} width={isPageWide ? 180 : 48} height={isPageWide ? 80 : 48} objectFit={'contain'} />
           </a>
         </Link>
